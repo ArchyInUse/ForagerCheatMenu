7005f7cd27e66a1ba68b44894e8b4adc60fd7cff
auxerre2b <archyinuse@gmail.com>
2025-10-20 21:04:45 +0300

    implemented process lookup by name to get the correct handle for the game

--- Injector/Injector.cpp
+++ Injector/Injector.cpp
@@ -9,12 +9,41 @@
 #include <vector>
 #include <algorithm>
 
+#define DEBUG 1
 using namespace std;
+
+vector<PROCESSENTRY32> listProcesses();
+DWORD findPidByName(const wstring& name);
+string getProcessImagePath(DWORD pid);
+string WCharToStr(WCHAR* wstr);
+bool enableDebugPrivilege();
 
 int main()
 {
     cout << "Forager DLL Injector" << endl;
-    cout << "Trying to find game...";
+    cout << "Changing privilege token..." << endl;
+    if (!enableDebugPrivilege())
+    {
+        string tmp;
+        cout << "Debug privileges denied, press any button to exit the program." << endl;
+        cin >> tmp;
+        return 1;
+    }
+    cout << "Trying to find game..." << endl;
+    auto processes = listProcesses();
+
+#if DEBUG
+    cout << "PID\tExeName\t\tPath" << endl;
+    cout << "------------------------------------------------------------" << endl;
+    for (auto& proc : processes)
+    {
+        string path = getProcessImagePath(proc.th32ProcessID);
+        cout << proc.th32ProcessID << "\t" << WCharToStr(proc.szExeFile) << "\t" << path << endl;
+    }
+#endif 
+
+
+    return 0;
 }
 
 vector<PROCESSENTRY32> listProcesses()
@@ -36,19 +65,84 @@ vector<PROCESSENTRY32> listProcesses()
     return list;
 }
 
-DWORD findPidByName(const string& name)
+DWORD findPidByName(wstring& name)
 {
     auto processes = listProcesses();
-    string lowerName = name;
+    wstring lowerName = name;
     
     // stack overflow (cursed) lambda for turning a string to lowercase
     transform(name.begin(), name.end(), name.begin(), [](unsigned char c) { return tolower(c); });
 
-    for (auto& entry : processes)
-    {
-        string exe = entry.szExeFile;
-         
+    for (auto& pe : processes) {
+        wstring exe = pe.szExeFile;
+        wstring lowerExe = exe;
+        transform(lowerExe.begin(), lowerExe.end(), lowerExe.begin(), [](unsigned char c) { return tolower(c); });
+        if (lowerExe == lowerName) {
+            return pe.th32ProcessID;
+        }
     }
+}
+
+string getProcessImagePath(DWORD pid)
+{
+    string path = "<access denied / system process>";
+    HANDLE hProc = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, 0, pid);
+    if (!hProc) return path;
+
+    char buf[MAX_PATH] = { 0 };
+    DWORD sz = MAX_PATH;
+
+    if (QueryFullProcessImageNameA(hProc, NULL, buf, &sz))
+    {
+        path = string(buf);
+    }
+    else {
+        if (GetModuleFileNameExA(hProc, 0, buf, MAX_PATH))
+        {
+            // fallback, most likely not going to be used but incase we have the PSAPI alternative
+            path = string(buf);
+        }
+    }
+
+    CloseHandle(hProc);
+    return path;
+}
+
+string WCharToStr(WCHAR* wstr)
+{
+    int bytesNeeded = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, nullptr, 0, nullptr, nullptr);
+    string result;
+    if (bytesNeeded > 0)
+    {
+        result.resize(bytesNeeded);
+        WideCharToMultiByte(CP_UTF8, 0, wstr, -1, &result[0], bytesNeeded, nullptr, nullptr);
+        if (!result.empty() && result.back() == '\0') result.pop_back();
+    } else {
+        result.clear();
+    }
+
+    return result;
+}
+
+bool enableDebugPrivilege() {
+    HANDLE hToken = nullptr;
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken))
+        return false;
+
+    TOKEN_PRIVILEGES tp;
+    LUID luid;
+    if (!LookupPrivilegeValue(nullptr, SE_DEBUG_NAME, &luid)) {
+        CloseHandle(hToken);
+        return false;
+    }
+
+    tp.PrivilegeCount = 1;
+    tp.Privileges[0].Luid = luid;
+    tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+    BOOL ok = AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(tp), nullptr, nullptr);
+    CloseHandle(hToken);
+    return ok && GetLastError() == ERROR_SUCCESS;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
